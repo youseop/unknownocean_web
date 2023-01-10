2750afe1c3b33281bcdccaca5d20fb11b80fd8cd
youseop <edlsw@naver.com>
2023-01-11 02:38:54 +0900

    distinguish IOS and Android & display link to go to the app store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">unknown ocean</div>;
+  const isIOS = () => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test(userAgent);
+  };
+
+  const userAgent = navigator.userAgent.toLowerCase();
+  return (
+    <div className="App">
+      <h1>unknown ocean</h1>
+      {isIOS() && (
+        <a
+          href="https://apps.apple.com/kr/app/unknown-ocean/id1639595747"
+          className="download_btn"
+        >
+          {"Download App (For IOS)"}
+        </a>
+      )}
+
+      {`userAgent : ${userAgent}`}
+
+      <a
+        href="https://apps.apple.com/kr/app/unknown-ocean/id1639595747"
+        className="download_btn"
+      >
+        {"Download App (test)"}
+      </a>
+    </div>
+  );
 }
 
 export default App;
