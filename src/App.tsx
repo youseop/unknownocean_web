2fc32c671690006d7126c7ba7a5f2ea6ec9a92d2
youseop <edlsw@naver.com>
2023-01-26 18:06:41 +0900

    make basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import "./css/App.scss";
+import Header from "./components/Header";
 
 function App() {
-  const isIOS = () => {
-    const userAgent = navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
-  };
-
-  const userAgent = navigator.userAgent.toLowerCase();
   return (
     <div className="App">
-      <h1>unknown ocean</h1>
-      {isIOS() && (
-        <a
-          href="https://apps.apple.com/kr/app/unknown-ocean/id1639595747"
-          className="download_btn"
-        >
-          {"Download App (For IOS)"}
-        </a>
-      )}
-
-      {`userAgent : ${userAgent}`}
-
-      <a
-        href="https://apps.apple.com/kr/app/unknown-ocean/id1639595747"
-        className="download_btn"
-      >
-        {"Download App (test)"}
-      </a>
+      <Header />
+      {/* pages */}
+      {/* meatballs */}
+      {/* button */}
+      {/* modal */}
     </div>
   );
 }
