68066dd7fb052cbf376c02d05b27aaf9f6049f61
youseop <edlsw@naver.com>
2023-03-07 17:46:01 +0900

    set ga id in env file

--- src/util/ga.ts
+++ src/util/ga.ts
@@ -1,9 +1,11 @@
 import ReactGA from "react-ga";
 
 export const initGa = () => {
-  const shouldDebug = false;
-  const gaTrackingId = "UA-255652251-2";
-  ReactGA.initialize(gaTrackingId, { debug: shouldDebug });
+  const shouldDebug = true;
+  const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID;
+  if (gaTrackingId) {
+    ReactGA.initialize(gaTrackingId, { debug: shouldDebug });
+  }
 };
 
 const sendGa = (category: string, action: string, label: string) => {
