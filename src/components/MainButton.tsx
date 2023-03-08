5264f96a7c48b57f1b2594b563f31b8e8f82fbfd
youseop <edlsw@naver.com>
2023-03-08 22:09:20 +0900

    Edit main button css and text

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -9,7 +9,7 @@ function MainButton({ onClickMainBtn }: MainButtonProps) {
     <div className="main-button-wrapper">
       <div className="button-background" />
       <div className="main-button" onClick={onClickMainBtn}>
-        MainButton
+        전시 관람하기
       </div>
     </div>
   );
