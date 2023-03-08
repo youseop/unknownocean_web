eca1207e1fdfedfdb4f8957dd852245be300a64a
youseop <edlsw@naver.com>
2023-03-08 18:00:19 +0900

    remove meatball render function

--- src/components/MainContents.tsx
+++ src/components/MainContents.tsx
@@ -10,29 +10,6 @@ interface MainContentsProps {
 }
 
 function MainContents({ pageNum, moveToSpecificPage }: MainContentsProps) {
-  const renderMeatBall = (pageNum: number) => {
-    const arr = new Array(LAST_PAGE_NUM);
-    for (let i = 0; i < arr.length; i++) arr[i] = i;
-
-    return (
-      <div className="meatball-wrapper">
-        {arr.map((_, i) => {
-          return (
-            <div
-              key={i}
-              className={classNames("meatball", {
-                current: i === pageNum,
-              })}
-              onClick={() => {
-                moveToSpecificPage(i);
-              }}
-            ></div>
-          );
-        })}
-      </div>
-    );
-  };
-
   return (
     <div className="main-contents">
       {pageNum === 0 ? <FirstPage /> : <SecondPage />}
